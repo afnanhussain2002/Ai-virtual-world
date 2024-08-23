764371294bdd617dca24acf90941759c1886020c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-23 11:11:02 +0600

    make a graph class in js

---
+++ js/math/graph.js
@@ -0,0 +1,11 @@
+class Graph {
+    constructor(points = [], segments = []){
+        this.points = points
+        this.segments = segments
+    }
+    draw(ctx) {
+        for(const seg of this.segments){
+            seg.draw(ctx)
+        }
+    }
+}
