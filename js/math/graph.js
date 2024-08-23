4ed8e0e161ec6a3b1f54eb3edd594bf7fe2d634f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-23 12:14:17 +0600

    add point func

--- js/math/graph.js
+++ js/math/graph.js
@@ -3,6 +3,10 @@ class Graph {
         this.points = points
         this.segments = segments
     }
+
+    addPoint(point){
+        this.points.push(point)
+    }
     draw(ctx) {
         for(const seg of this.segments){
             seg.draw(ctx)
